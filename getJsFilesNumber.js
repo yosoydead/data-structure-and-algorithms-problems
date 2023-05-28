f9358525168b1513fc69b184ba1d6dd9e4e33166
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-05-29 00:02:40 +0300

    schimbat post commit iar

--- getJsFilesNumber.js
+++ getJsFilesNumber.js
@@ -22,59 +22,61 @@ const generateMarkdownText = (numOfJsFiles) => {
  * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
  * @example const output = await execute("ls -alh");
  */
-// function execute(command) {
-//   /**
-//    * @param {Function} resolve A function that resolves the promise
-//    * @param {Function} reject A function that fails the promise
-//    * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-//    */
-//   return new Promise(function(resolve, reject) {
-//     /**
-//      * @param {Error} error An error triggered during the execution of the childProcess.exec command
-//      * @param {string|Buffer} standardOutput The result of the shell command execution
-//      * @param {string|Buffer} standardError The error resulting of the shell command execution
-//      * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
-//      */
-//     childProcess.execSync(command, function(error, standardOutput, standardError) {
-//       if (error) {
-//         reject();
-
-//         return;
-//       }
-
-//       if (standardError) {
-//         reject(standardError);
-
-//         return;
-//       }
-
-//       resolve(standardOutput);
-//     });
-//   });
-// }
-
 function execute(command) {
-  childProcess.execSync(command, function(error, standardOutput, standardError) {
-    if (error) {
-      // reject();
-      // return;
-      throw new Error(`eroare simpla ${error}`);
-    }
-  
-    if (standardError) {
-      // reject(standardError);
-      throw new Error(`Standard error simpla ${standardError}`);
-      // return;
-    }
-      
-    return standardOutput;
+  /**
+   * @param {Function} resolve A function that resolves the promise
+   * @param {Function} reject A function that fails the promise
+   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+   */
+  return new Promise(function(resolve, reject) {
+    /**
+     * @param {Error} error An error triggered during the execution of the childProcess.exec command
+     * @param {string|Buffer} standardOutput The result of the shell command execution
+     * @param {string|Buffer} standardError The error resulting of the shell command execution
+     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+     */
+    childProcess.execFile(command, [], {shell: true}, function(...args) {
+      console.log("args", args);
+      if (args[0]) {
+        reject(args[0]);
+
+        return;
+      }
+
+      if (args[2]) {
+        reject(args[2]);
+
+        return;
+      }
+
+      resolve(args[1]);
+    });
   });
 }
 
-function main() {
+// function execute(command) {
+//   childProcess.execSync(command, function(error, standardOutput, standardError) {
+//     if (error) {
+//       // reject();
+//       // return;
+//       throw new Error(`eroare simpla ${error}`);
+//     }
+  
+//     if (standardError) {
+//       // reject(standardError);
+//       throw new Error(`Standard error simpla ${standardError}`);
+//       // return;
+//     }
+      
+//     return standardOutput;
+//   });
+// }
+
+async function main() {
   console.log("post commit trigger");
   try {
-    const jsFileCount = execute('find . -mindepth 1 -type f -name "*.js" -printf x | wc -c');
+    const jsFileCount = await execute('find . -mindepth 1 -type f -name "*.js" -printf x | wc -c');
+    await execute("git status");
 
     console.log(`Am gasit ${jsFileCount} fisiere de JS. O sa scad 1 din cauza scriptului de post commit.`);
     const text = generateMarkdownText(parseInt(jsFileCount-1));
@@ -85,8 +87,9 @@ function main() {
       }
       console.log("The file was saved!");
     });
-     execute("git add .");
-    execute('git commit -m "POST-COMMIT: adaug fisiere ramase. update pe readme.md"');
+
+    await execute("git add .");
+    await execute('git commit -m "POST-COMMIT: adaug fisiere ramase. update pe readme.md"');
   } catch (error) {
     console.error(error.toString());
   }
