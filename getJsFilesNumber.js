b4568297c78da99bf4362409e172fc4c53d546e6
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-05-28 22:09:45 +0300

    test commit. schimbat commit hook

--- getJsFilesNumber.js
+++ getJsFilesNumber.js
@@ -22,40 +22,58 @@ const generateMarkdownText = (numOfJsFiles) => {
  * @return {Promise<string>} A promise that resolve to the output of the shell command, or an error
  * @example const output = await execute("ls -alh");
  */
+// function execute(command) {
+//   /**
+//    * @param {Function} resolve A function that resolves the promise
+//    * @param {Function} reject A function that fails the promise
+//    * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+//    */
+//   return new Promise(function(resolve, reject) {
+//     /**
+//      * @param {Error} error An error triggered during the execution of the childProcess.exec command
+//      * @param {string|Buffer} standardOutput The result of the shell command execution
+//      * @param {string|Buffer} standardError The error resulting of the shell command execution
+//      * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
+//      */
+//     childProcess.exec(command, function(error, standardOutput, standardError) {
+//       if (error) {
+//         reject();
+
+//         return;
+//       }
+
+//       if (standardError) {
+//         reject(standardError);
+
+//         return;
+//       }
+
+//       resolve(standardOutput);
+//     });
+//   });
+// }
+
 function execute(command) {
-  /**
-   * @param {Function} resolve A function that resolves the promise
-   * @param {Function} reject A function that fails the promise
-   * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-   */
-  return new Promise(function(resolve, reject) {
-    /**
-     * @param {Error} error An error triggered during the execution of the childProcess.exec command
-     * @param {string|Buffer} standardOutput The result of the shell command execution
-     * @param {string|Buffer} standardError The error resulting of the shell command execution
-     * @see https://nodejs.org/api/child_process.html#child_process_child_process_exec_command_options_callback
-     */
-    childProcess.exec(command, function(error, standardOutput, standardError) {
-      if (error) {
-        reject();
-
-        return;
-      }
-
-      if (standardError) {
-        reject(standardError);
-
-        return;
-      }
-
-      resolve(standardOutput);
-    });
+  childProcess.exec(command, function(error, standardOutput, standardError) {
+    if (error) {
+      // reject();
+      // return;
+      throw new Error(`eroare simpla ${error}`);
+    }
+  
+    if (standardError) {
+      // reject(standardError);
+      throw new Error(`Standard error simpla ${standardError}`);
+      // return;
+    }
+      
+    return standardOutput;
   });
 }
 
-async function main() {
+function main() {
   try {
-    const jsFileCount = await execute('find . -mindepth 1 -type f -name "*.js" -printf x | wc -c');
+    const jsFileCount = execute('find . -mindepth 1 -type f -name "*.js" -printf x | wc -c');
 
     console.log(jsFileCount);
     const text = generateMarkdownText(parseInt(jsFileCount));
@@ -66,7 +84,7 @@ async function main() {
       }
       console.log("The file was saved!");
     });
-    await execute("git add .");
+    execute("git add .");
   } catch (error) {
     console.error(error.toString());
   }
