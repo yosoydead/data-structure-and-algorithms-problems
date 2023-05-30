ce668463c062efa0a83feac07f86c3fa7f78f256
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-05-30 18:47:55 +0300

    probleme 6 kyu

---
+++ Code Wars/6 kyu/Highest Scoring Word/a.js
@@ -0,0 +1,37 @@
+/**
+ * Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+ */
+function high(x){
+  const m = {};
+  
+  const arr = x.split(" ");
+  let biggest = -10000;
+
+  for (let i = 0; i < arr.length; i++) {
+    const x = arr[i];
+    let s = 0;
+    for (let j = 0; j < x.length; j++) {
+      s += (x.charCodeAt(j) - 96);
+    }
+    
+    if (m[s]) {
+      m[s].push(x);
+    } else {
+      m[s] = [x];
+    }
+    
+    if (s > biggest) { biggest = s; }
+  }
+  
+  return m[biggest][0];
+}
---
+++ Code Wars/6 kyu/Word a10n (abbreviation)/a.js
@@ -0,0 +1,65 @@
+/**
+ * The word i18n is a common abbreviation of internationalization in the developer community, used instead of typing the whole word and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+
+Write a function that takes a string and turns any and all "words" (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+
+A "word" is a sequence of alphabetical characters. By this definition, any other character like a space or hyphen (eg. "elephant-ride") will split up a series of letters into two words (eg. "elephant" and "ride").
+The abbreviated version of the word should have the first letter, then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+Example
+abbreviate("elephant-rides are really fun!")
+//          ^^^^^^^^*^^^^^*^^^*^^^^^^*^^^*
+// words (^):   "elephant" "rides" "are" "really" "fun"
+//                123456     123     1     1234     1
+// ignore short words:               X              X
+
+// abbreviate:    "e6t"     "r3s"  "are"  "r4y"   "fun"
+// all non-word characters (*) remain in place
+//                     "-"      " "    " "     " "     "!"
+=== "e6t-r3s are r4y fun!"
+ */
+function transform(str) {
+  let res = "";
+  let count = 1;
+  
+  for(let i = 1; i < str.length - 2; i++) {
+    count++;
+  }
+  res = `${str[0]}${count}${str[str.length-1]}`;
+  
+  return res;
+}
+
+function abbreviate(string) {
+  let word = "";
+  let sent = "";
+
+  const arr = [];
+  for (let i = 0; i < string.length; i++) {
+    // const r = arr[i].match(regex);
+    // console.log(r);.
+    if (string[i].match(/[a-z]/i)) {
+      console.log("litera", string[i]);
+      word += string[i];
+    } else {
+      console.log("nu litera");
+      arr.push(word);
+      arr.push(string[i]);
+      word = "";
+    }
+  }
+  arr.push(word);
+
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > 3) {
+      sent += transform(arr[i]);
+    } else {
+      sent += arr[i].toString();
+    }
+  }
+
+  return sent;
+}
+
+abbreviate("elephant-ride")
