ab0ee5da11099a63e5c9a7ea4a992dfab51e5560
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-06-04 02:41:21 -0700

    POST-COMMIT: adaug fisiere ramase. update pe readme.md

---
+++ Tudor Sorin - Clasa a 10-a/Cap. 6/Liste liniare alocate simplu inlantuit/1.js
@@ -0,0 +1,21 @@
+/**
+ * Fiind data o lista liniara simplu inlantuita, cu adresa de inceput V,
+ * se cere sa se inverseze legaturile din lista, adica daca in lista initiala,
+ * dupa nodul I urmeaza I+1, atunci, noua lista, dupa nodul I+1, urmeaza I.
+ * 
+ * R: Functia INV, rezolva problema data. Ea are 2 parametri: adresa primului
+ * element al listei si adresa urmatorului element. Practic, la fiecare pas, partea
+ * de adresa a nodului referit de curent, va retine adresa referita de pred. Functia
+ * returneaza adresa primului nod al listei inversate, adica a ultimului nod in cazul
+ * listei neinversate. Inainte de apelul functiei, partea de adresa a primului nod va
+ * trebui sa retina null/nil.
+ */
+class Node {
+    constructor(val, next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+const a = new Node(1, new Node(2, new Node(3, new Node(4))));
+console.log(a);
