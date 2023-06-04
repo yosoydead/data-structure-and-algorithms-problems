897b1c62dc64b4950ce8af9faaa24cd21bc8f44d
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-06-04 06:32:47 -0700

    cateva hackerrank easy

---
+++ HackerRank/Easy/Simple array sum/a.js
@@ -0,0 +1,21 @@
+/**
+ * Given an array of integers, find the sum of its elements.
+
+For example, if the array , , so return .
+
+Function Description
+
+Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers
+Input Format
+
+The first line contains an integer, , denoting the size of the array.
+The second line contains  space-separated integers representing the array's elements.
+*/
+function simpleArraySum(ar) {
+    // Write your code here
+    return ar.reduce((acc, num) => acc + num, 0);
+}
---
+++ HackerRank/Easy/Solve me first/a.js
@@ -0,0 +1,19 @@
+/**Complete the function solveMeFirst to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the solveMeFirst function in the editor below.
+
+solveMeFirst has the following parameters:
+
+int a: the first value
+int b: the second value
+Returns
+- int: the sum of  and 
+*/
+const solveMeFirst = (a, b) => a + b;
