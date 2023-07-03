6fa9f20fb0502481735f2c5a3c9a0e1071ec05e9
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2023-07-03 20:58:37 +0300

    leet code. problema easy LL

---
+++ LeetCode/Easy/234. Palindrome Linked List/a.js
@@ -0,0 +1,60 @@
+/**
+ * 234. Palindrome Linked List
+Easy
+14.4K
+797
+Companies
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let s = "";
+    while (head) {
+        s += head.val;
+        head = head.next;
+    }
+
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+        if (s[i] !== s[j]) { return false; }
+        i++;
+        j--;
+    }
+
+    return true;
+};
